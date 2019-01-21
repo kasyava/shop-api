6f74b1bef6304a38047f9d9e6092c664b4fcc913
kasyava <budo_slava@mail.ru>
2019-01-21 14:37:02 +0600

    создали директорию public и начали подготовку логики закгрузки файлов на сервер

--- app/product.js
+++ app/product.js
@@ -1,5 +1,8 @@
 //const express = require("express");
 import express from "express";
+import multer from "multer";
+import path from "path";
+
 const router = express.Router();
 
 const fsDemo = require("../fsDemo");
@@ -22,12 +25,15 @@ router.get('/:id', (req, res) =>{
 
 router.post('/', (req, res) =>{
     console.log(req.body);
-    fsDemo.addItem(data, req.body);
+
+
+    res.send('ok');
+    /*fsDemo.addItem(data, req.body);
 
     fsDemo.saveData(data, ()=>{
         res.send(data);
     });
-
+*/
 });
 
 export default router;
---
+++ config.js
@@ -0,0 +1,9 @@
+import path from "path";
+
+
+const rootPath = __dirname;
+
+export default {
+    rootPath,
+    uploadPath: path.join(rootPath, "public/uploads")
+}
